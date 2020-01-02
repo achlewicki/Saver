cda792952ceab002a0cb9d41618b3ba58878af8e
Daniel Bińkowski <daniel1binkowski@gmail.com>
2020-01-02 22:08:08 +0100

    Szkielet okna i podstawowe funkcjonalności

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'svr-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'projekt';
+  title = 'project';
 }
