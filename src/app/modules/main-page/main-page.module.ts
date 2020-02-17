bab36f8564e976303688d638f9ff325be25e52b4
Daniel Bińkowski <daniel1binkowski@gmail.com>
2020-02-17 01:43:37 +0100

    Szablon głównej strony aplikacji

--- src/app/modules/main-page/main-page.module.ts
+++ src/app/modules/main-page/main-page.module.ts
@@ -5,12 +5,13 @@ import { MainPageRoutingModule } from './main-page-routing.module';
 import { MainPageCoreComponent } from './_main-page-core/main-page-core.component';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
-
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    MainPageCoreComponent
+    MainPageCoreComponent,
+    NavBarComponent
   ],
   imports: [
     CommonModule,
