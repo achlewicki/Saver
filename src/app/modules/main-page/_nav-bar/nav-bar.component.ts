0944b93972c2b0617049a6ee111494469b19debc
Daniel Bińkowski <daniel1binkowski@gmail.com>
2020-02-17 01:43:37 +0100

    Szablon głównej strony aplikacji

---
+++ src/app/modules/main-page/_nav-bar/nav-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'svr-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.scss']
+})
+export class NavBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/modules/main-page/main-page.module.ts
+++ src/app/modules/main-page/main-page.module.ts
@@ -5,12 +5,15 @@ import { MainPageRoutingModule } from './main-page-routing.module';
 import { MainPageCoreComponent } from './_main-page-core/main-page-core.component';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
-
+import { NavBarComponent } from './_nav-bar/nav-bar.component';
+import { HeaderComponent } from './_header/header.component';
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    MainPageCoreComponent
+    MainPageCoreComponent,
+    NavBarComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule,
