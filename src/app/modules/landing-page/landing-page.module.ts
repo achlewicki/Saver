c9d574514751b933f41b8f7ea5de606c4292cf5f
ForceViper <chlewickiadam@gmail.com>
2020-03-04 20:23:27 +0100

    Poprawki strony startowej

--- src/app/modules/landing-page/landing-page.module.ts
+++ src/app/modules/landing-page/landing-page.module.ts
@@ -10,6 +10,7 @@ import { LandingNavBarComponent } from './landing-nav-bar/landing-nav-bar.compon
 import { RegisterModule } from './register/register.module';
 import { LoginModule } from './login/login.module';
 import { ContentComponent } from './content/content.component';
+import {MatButtonModule} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -17,12 +18,13 @@ import { ContentComponent } from './content/content.component';
     LandingPageCoreComponent,
     LandingNavBarComponent
   ],
-  imports: [
-    RegisterModule,
-    LoginModule,
-    CommonModule,
-    RouterModule,
-    LandingPageRoutingModule
-  ]
+    imports: [
+        RegisterModule,
+        LoginModule,
+        CommonModule,
+        RouterModule,
+        LandingPageRoutingModule,
+        MatButtonModule
+    ]
 })
 export class LandingPageModule { }
