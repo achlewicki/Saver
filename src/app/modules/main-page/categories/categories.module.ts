970a0f89c5f10d82dcb9bc7bbbb9b17125b9bc1d
ForceViper <chlewickiadam@gmail.com>
2020-03-07 16:20:29 +0100

    Dodanie widoku Kategorii

--- src/app/modules/main-page/categories/categories.module.ts
+++ src/app/modules/main-page/categories/categories.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {CategoriesComponent} from './categories/categories.component';
 import {FlexModule} from '@angular/flex-layout';
+import { EditCategoryComponent } from './edit-category/edit-category.component';
 
 
 
 @NgModule({
   declarations: [
-    CategoriesComponent
+    CategoriesComponent,
+    EditCategoryComponent
   ],
   imports: [
     CommonModule,
--- src/app/modules/main-page/categories/categories/categories.component.ts
+++ src/app/modules/main-page/categories/categories/categories.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { HeaderService } from '#services/header-service/header.service';
 import { ActivatedRoute } from '@angular/router';
 import { map } from 'rxjs/operators';
-import { MatDialog } from '@angular/material';
-
 
 @Component({
   selector: 'svr-categories',
@@ -13,24 +11,24 @@ import { MatDialog } from '@angular/material';
 export class CategoriesComponent implements OnInit {
   // private loopTimes: number;
   Categories = [
-    {name: 'Category 1', color: 'red'},
-    {name: 'Category 2'},
-    {name: 'Category 3'},
-    {name: 'Category 1', color: 'blue'},
-    {name: 'Category 2'},
-    {name: 'Category 3'},
-    {name: 'Category 1', color: 'yellow'},
-    {name: 'Category 2'},
-    {name: 'Category 3'},
-    {name: 'Category 1', color: 'green'},
-    {name: 'Category 2'},
-    {name: 'Category 3'},
+    {id: 1, name: 'Category 1', color: 'red'},
+    {id: 2,name: 'Category 2'},
+    {id: 3,name: 'Category 3'},
+    {id: 4,name: 'Category 1', color: 'blue'},
+    {id: 5,name: 'Category 2'},
+    {id: 6,name: 'Category 3'},
+    {id: 7,name: 'Category 1', color: 'yellow'},
+    {id: 8,name: 'Category 2'},
+    {id: 9,name: 'Category 3'},
+    {id: 10,name: 'Category 1', color: 'green'},
+    {id: 11,name: 'Category 2'},
+    {id: 12,name: 'Category 3'},
   ];
 
   public primary: string;
+  private currentId: number;
 
   constructor(
-    public dialog: MatDialog,
     private hservice: HeaderService,
     private route: ActivatedRoute
   ) {
@@ -42,8 +40,8 @@ export class CategoriesComponent implements OnInit {
       .subscribe((x) => this.hservice.viewTitle.next(x));
   }
 
-  openDialog(){
-    this.dialog.open();
+  private setCurrentId(id: number){
+    this.currentId = id;
   }
 }
 
